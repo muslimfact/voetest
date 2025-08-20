b78f1926ace23ddb18edcf7ab5221cc0ffee74e4
muslimfact <muslimfact900@gmail.com>
2025-08-20 18:19:04 +0700

    Update dl.py

--- dl.py
+++ dl.py
@@ -22,20 +22,20 @@ from urllib.parse import urlparse
 #]
 USER_AGENTS = [
     # Chrome di Windows 10
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+#    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+#    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 
     # Chrome di Android 10
     "Mozilla/5.0 (Linux; Android 10; SM-G973F) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
 
     # Chrome di macOS
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+#    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+#    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 
     # Chrome di Linux
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+#    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+#    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 ]
 
 
